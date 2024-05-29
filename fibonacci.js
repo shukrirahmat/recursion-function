0affe676cfb9e03f73d2ca917fb3a28cca3d5e82
Shukri Rahmat <119567950+shukrirahmat@users.noreply.github.com>
2024-05-29 19:29:38 +0800

    make it PRETTIER

--- fibonacci.js
+++ fibonacci.js
@@ -1,35 +1,33 @@
 function fibs(n) {
+  let result = [];
+  let count = 0;
 
-    let result = [];
-    let count = 0;
-
-    while (count < n) {
-        if (count == 0) {
-            result.push(0);
-        } else if (count == 1) {
-            result.push(1);
-        } else {
-            const value = result[count - 1] + result[count - 2];
-            result.push(value);
-        }
-        count++;
+  while (count < n) {
+    if (count == 0) {
+      result.push(0);
+    } else if (count == 1) {
+      result.push(1);
+    } else {
+      const value = result[count - 1] + result[count - 2];
+      result.push(value);
     }
+    count++;
+  }
 
-    return result;
+  return result;
 }
 
 function fibsRec(n) {
+  if (n === 1) {
+    return [0];
+  }
 
-    if (n === 1) {
-        return [0];
-    }
+  if (n === 2) {
+    return [0, 1];
+  }
 
-    if (n === 2) {
-        return [0, 1];
-    }
-
-    let previous = fibsRec(n - 1);
-    return previous.concat([previous.at(-1) + previous.at(-2)]); 
+  let previous = fibsRec(n - 1);
+  return previous.concat([previous.at(-1) + previous.at(-2)]);
 }
 
-export {fibs, fibsRec};
+export { fibs, fibsRec };
